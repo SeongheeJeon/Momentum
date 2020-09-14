31dabfdc5973ea22d0ccbdbc1d1803f07e9078c9
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2020-09-14 21:07:09 +0900

    Add delete button and background

--- todo.js
+++ todo.js
@@ -4,17 +4,46 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+let toDos = [];
+
+
+// delete버튼을 누르면 localstorage에서 삭제되고, 
+// localstorage를 다시 로드하도록 하면 두번작업하지 않아도 되나?
+function deleteTodo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerHTML = "❌";
     const span = document.createElement("span");
+    const newId = toDos.length +1;
+    delBtn.innerHTML = "❌";
+    delBtn.addEventListener("click", deleteTodo);
     span.innerHTML = text;
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
+// 제출을 하면 바로 로컬에 저장하고, 그것을 load시키도록 구현해보기
+// -> 이렇게하면 매번 새로 모든 localstorage 데이터를 로드해야해서 오래걸리나?
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = toDoInput.value;
@@ -23,9 +52,17 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    
+    /* getItem으로 받아온 값은 하나의 string임을 확인
+    console.log("[hello]");
+    console.log(loadedToDos);
+    console.log(loadedToDos[0]); */
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text)
+        })
     }
 }
 
